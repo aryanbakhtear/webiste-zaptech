5d3cc21ab09f94711ab109d36b15f12e6a37365d
aryanbakhtear <aryanbakhtear@gmail.com>
2025-06-19 05:26:21 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+window.PC_APPS = [
+  {
+    title: "Plutonium",
+    desc: "A custom Call of Duty client. Play classic games with new features and anti-cheat. Official site download.",
+    image: "https://dn721308.ca.archive.org/0/items/co-d-black-ops-cover/platunuim.png",
+    link: "https://plutonium.pw/"
+  },
+  {
+    title: "Black Ops 1",
+    desc: "Download Call of Duty: Black Ops 1 full game torrent (Plutonium T5).",
+    image: "https://dn721308.ca.archive.org/0/items/co-d-black-ops-cover/CoD_Black_Ops_cover.png",
+    link: "https://web.archive.org/web/20230317230359/https://plutonium.pw/pluto_t5_full_game.torrent"
+  },
+  {
+    title: "Black Ops 2",
+    desc: "Download Call of Duty: Black Ops 2 full game torrent (Plutonium T6).",
+    image: "https://ia800901.us.archive.org/3/items/co-d-black-ops-cover/Call_of_Duty_Black_Ops_II_.png",
+    link: "https://web.archive.org/web/20220603191144/https://plutonium.pw/pluto_t6_full_game.torrent"
+  },
+  {
+    title: "Visual Code",
+    desc: "Powerful code editor for developers",
+    image: "https://archive.org/download/dwada/dwada.png",
+    link: "https://mega.nz/file/ipxC1QKb#AT4lBbG86n-RzhoUHXGb0_EyF8mDtKvU3EFKBeQkq24"
+  },
+  {
+    title: "Code Block",
+    desc: "perfect app for c++",
+    image: "https://ia600904.us.archive.org/27/items/untitled-design_20250612/Untitled%20design.png",
+    link: "https://mega.nz/file/G0hHHKQR#wwFw2l4FNMxgGT3-rHfxZe18WkwSnjleUMgUL6E4_rc"
+  },
+  {
+    title: "Adobe Photoshop",
+    desc: "Professional image editing software 2019",
+    image: "https://ia600901.us.archive.org/10/items/photoshop_202506/photoshop.png",
+    link: "https://mega.nz/file/bkBVgDJa#5xGuD2Ieps7SMnzpkpRA_VVbbkoC6bzZdTKa63Ls2EI"
+  },
+  {
+    title: "Adobe Premiere Pro",
+    desc: "Professional video editing software 2023",
+    image: "https://archive.org/download/dsgfsrf/dsgfsrf.png",
+    link: "https://mega.nz/file/ThZW2aoB#N0jMQInKg2gqQxD2zjXZ79aURD0ZhyJ5B0knjr6wCPw"
+  }
+]; 
